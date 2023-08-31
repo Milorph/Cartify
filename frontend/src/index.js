8e349556eb101d837ff680f689d857647044b608
robert <robertwinstonwidjaja1@gmail.com>
2023-08-31 13:10:41 +0700

    Added redux slicing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -26,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
