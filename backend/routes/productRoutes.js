b2a337f913c87f09942441fb8f6eb76257762bf8
robert <robertwinstonwidjaja1@gmail.com>
2023-09-23 12:34:21 -0700

    admin deletion of product routes and functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
   getProducts,
   createProduct,
   updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -13,7 +14,10 @@ router
   .get(getProducts)
   .post(protect, admin, createProduct, createProduct);
 
-router.route("/:id").get(getProductById);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
